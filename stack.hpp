b997bf79ae39c3cae1c939a9395c3f0fd487234a
tommymcroskey <mcroskey.thomas@gmail.com>
2026-04-28 16:39:09 -0600

    create template class structure for stack

---
+++ stack.hpp
@@ -0,0 +1,17 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#ifdef DEBUG
+#include <iostream>
+#endif
+
+template <typename T, std::size_t N>
+class Stack {
+
+public:
+
+private:
+
+};
+
+#endif // STACK_HPP
