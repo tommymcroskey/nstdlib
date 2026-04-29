47065dfee01d5135a8dd1c69df0591094855a048
tommymcroskey <mcroskey.thomas@gmail.com>
2026-04-28 20:51:49 -0600

    circular buffer template class definition

---
+++ circular_buffer.cpp
@@ -0,0 +1,53 @@
+#ifndef CIRCULAR_BUFFER_HPP
+#define CIRCULAR_BUFFER_HPP
+
+#include <cstddef>
+#include <cstdlib>
+
+template <typename T, size_t N>
+class CircularBuffer {
+
+public:
+
+CircularBuffer(size_t capacity) :
+	capacity_(capacity),
+	size_(0),
+	front_(0),
+	back_(0)
+{}
+~CircularBuffer() = default;
+
+void enqueue(T e) {
+	if (full()) {
+		throw std::overflow_error("circular buffer is full");
+	}
+	data[front_] = std::move(e);
+	front_ = (front_ + 1) % capacity_;
+	size_++;
+}
+
+T dequeue() {
+	if (empty()) {
+		throw std::underflow_error("circular buffer is empty");
+	}
+	size_t to_pop = back_;
+	back_ = (back_ + 1) % capacity_;
+	size_--;
+	return std::move(data[to_pop]);
+}
+
+size_t capacity() const { return capacity_; }
+size_t size() const { return size_; }
+bool empty() const { return size_ == capacity_; }
+bool full() const { return size_ == 0; }
+
+private:
+
+size_t capacity_;
+size_t size_;
+size_t front_;
+size_t back_;
+
+T data[N];
+
+#endif // CIRCULAR_BUFFER_HPP 
---
+++ test_cbuf.cpp
@@ -0,0 +1,11 @@
+#include "circular_buffer.hpp"
+
+#include <cassert>
+#include <stdexcept>
+#include <cstddef>
+
+void test_constructor_sanity() {
+	CircularBuffer<int, 10> cb;
+}
+
+
