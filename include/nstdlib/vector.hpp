ddbaf2873838dbb92bd0c6279e16a9033bf0bbc1
tommymcroskey <mcroskey.thomas@gmail.com>
2026-05-05 14:15:55 -0600

    non working vector.hpp additions

--- include/nstdlib/vector.hpp
+++ include/nstdlib/vector.hpp
@@ -1,6 +1,7 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
+#include <cstdlib>
 #include <cstddef>
 #include <memory>
 
@@ -24,11 +25,29 @@ public:
 		, size_(0)
 	{}
 
-	Vector(size_t capacity, const T& fill);
-	Vector(const Vector& o);
-	Vector(Vector&& o) noexcept;
+	Vector(size_t capacity, const T& fill) : Vector(capacity) {
+		for (; size_ < capacity_; size_++) {
+			data_[size_] = fill;
+		}
+	}
+		
+	Vector(const Vector& o) : Vector(o.capacity_) {
+		for (; size_ < capacity_; size_++) {
+			data_[size_] = o[size_];
+		}
+	}
+	Vector(Vector&& o) noexcept
+		: data_(std::move(o.data_))
+		, capacity(o.capacity_)
+		, size_(o.size_)
+	{}
 
-	~Vector();
+	~Vector() {
+		for (T& e : data_) {
+			T::delete e;
+		}
+		::operator delete data_;
+	}
 
 	Vector& operator=(const Vector& o);
 	Vector& operator=(Vector&& o);
@@ -41,7 +60,7 @@ public:
 		if (_out_of_bounds(idx)) {
 			throw std::out_of_range("index not in range");
 		}
-		return data_.get()[idx];
+		return data_[idx];
 	}
 
 	/**
@@ -72,19 +91,57 @@ public:
 		return *this[size_ - 1]
 	}
 	size_t size() const { return size_; }
-	void push_back(const T& e);
+	void push_back(const T& e) {
+		if (_full()) {
+			_expand_exponential();
+		}
+		data_[size_++] = e;
+	}
 
 private:
 
 	static constexpr default_capacity = 8;
 
-	unique_ptr<T> data_;
+	T* data_;
 	size_t capacity_;
 	size_t size_;
 
 	bool _empty() const { return size_ == 0; }
+	bool _full() const { return size_ == capacity_; }
 	bool _out_of_bounds(size_t idx) const { return idx >= size_; }
-	void _expand_exponential();
+	void _expand() {
+		if (capacity_ * 2 < capacity) {
+			throw std::bad_alloc;
+		}
+		capacity_ *= 2;
+		if (std::is_trivially_copyable<T>::value) {
+			_tv_expand_exponential();
+		} else {
+			_ud_expand_exponential();
+		}
+	}
+	/**
+	* @brief user defined expand 2^n capacity for n usages
+	*/
+	void _ud_expand_exponential() {
+		T* ptr = ::operator new(capacity_ * sizeof(T));	
+		for (size_t i = 0; i < size_; i++) {
+			ptr[i] = data_[i];
+			T::delete data_[i];
+		}
+		::operator delete data_;
+		data_ = ptr;
+	}
+	/**
+	* @brief trivially copyable expand 2^n capacity for n usage, using realloc optimization
+	*/
+	void _tc_expand_exponential() {
+		T* ptr = realloc((void*)data_, capacity_);
+		if (ptr == nullptr) {
+			throw std::bad_alloc;
+		}
+	}
+		
 };
 
 };
